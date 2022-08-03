aa3f0f35a326c9a492272827fabcdf2f0e21f2c9
sevinjshabanova2006 <shabanovasevinc2006@gmail.com>
2022-08-03 16:04:55 +0400

    sevinc

--- Baku servis/demo03.houzez.co/main3.js
+++ Baku servis/demo03.houzez.co/main3.js
@@ -1,7 +1,9 @@
-function contact(){
-  swal(
-    'Sorğunuz uğurla yerinə yetirildi!',
-    'You clicked the button!',
-    'success'
-  )
-}
+function openNav() {
+    document.getElementById("mySidebar").style.width = "300px";
+    document.getElementById("main").style.marginRight = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginRight= "0";
+  }
